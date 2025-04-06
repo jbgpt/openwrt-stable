1e4bba3d1c6e349a3f7085ab218a732ee3e38b5b
jbgpt <134644850+jbgpt@users.noreply.github.com>
2025-04-06 21:33:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
@@ -8,13 +7,21 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i -e "s/set system\.@system$-1$\.zonename='UTC'/set system\.@system$-1$\.timezone='CST-8'/" \
+ #      -e "a\set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate# Modify default IP
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i '/option band.*2g.*/{s|option ssid.*|option ssid "openwrt_2G"|}' package/base-files/files/bin/config_generate
+#sed -i '/option band.*5g.*/{s|option ssid.*|option ssid "openwrt_5G"|}' package/base-files/files/bin/config_generate
+#sed -i '/option band.*5g.*/{s|option country.*|option country "US"|}' package/base-files/files/bin/config_generate
+#sed -i '/option band.*2g.*/{s|option disabled.*|option disabled 0|}' package/base-files/files/bin/config_generate
+#sed -i '/option band.*5g.*/{s|option disabled.*|option disabled 0|}' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/office-open/g' package/base-files/files/bin/config_generate
+echo "net.core.default_qdisc=fq" >> package/base-files/files/etc/sysctl.conf
+
+
+echo "DIY2: 系统配置已预置（时区、IP、WiFi）"
